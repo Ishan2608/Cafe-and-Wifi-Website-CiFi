8ddf758f07695df15c761efc360219ae7497c45b
IshanRastogi2608 <ishanrastogi26@gamil.com>
2022-06-21 21:20:45 +0530

    Only Authenticated People Can Delete and Add Cafes

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, SelectField, TimeField
+from wtforms import StringField, SubmitField, SelectField, TimeField, PasswordField
 from wtforms.validators import DataRequired, URL
 from flask_sqlalchemy import SQLAlchemy
+
+rating_options = ['🤬', '😢😢', '😊😊😊', '😃😃😃😃', '🤩🤩🤩🤩🤩']
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'my_secret_key_is_kept_in_my___'
 
-app.config['SQLAlchemy_DATABASE_URI'] = 'sqlite:////tmp/cifi.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cifi.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
@@ -19,6 +21,7 @@ class Cafe(db.Model):
     closing_time = db.Column(db.Time)
     coffee_rating = db.Column(db.Integer, nullable=False)
     wifi_rating = db.Column(db.Integer, nullable=False)
+    toilet_rating = db.Column(db.Integer, nullable=False)
     location = db.Column(db.String(250))
 
 
@@ -26,11 +29,13 @@ db.create_all()
 
 
 class CifiForm(FlaskForm):
+    secret_key = PasswordField('Secret Key', validators=[DataRequired()])
     cafe_name = StringField("Cafe Name", validators=[DataRequired()])
     opening_time = TimeField('Opening Time')
     closing_time = TimeField('Closing Time')
-    coffee_rating = SelectField('Coffee Rating', choices=['1', '2', '3', '4', '5'], validators=[DataRequired()])
-    wifi_rating = SelectField('Wifi Rating', choices=['1', '2', '3', '4', '5'], validators=[DataRequired()])
+    coffee_rating = SelectField('Coffee Rating', choices=rating_options, validators=[DataRequired()])
+    wifi_rating = SelectField('Wifi Rating', choices=rating_options, validators=[DataRequired()])
+    toilet_rating = SelectField('Toilet Rating', choices=rating_options, validators=[DataRequired()])
     location = StringField('Location', validators=[DataRequired(), URL()])
     submit = SubmitField('Add Cafe')
 
@@ -47,19 +52,35 @@ def insert_new():
     form = CifiForm()
     if form.validate_on_submit():
 
-        new_cafe = Cafe(
-            cafe_name=form.cafe_name.data,
-            opening_time=form.opening_time.data,
-            closing_time=form.closing_time.data,
-            coffee_rating=form.coffee_rating.data,
-            wifi_rating=form.wifi_rating.data,
-            location=form.location.data
-        )
-        db.session.add(new_cafe)
-        db.session.commit()
+        if form.secret_key.data == 'i9s19h8a1n14':
+            new_cafe = Cafe(
+                cafe_name=form.cafe_name.data, opening_time=form.opening_time.data,
+                closing_time=form.closing_time.data, coffee_rating=form.coffee_rating.data,
+                wifi_rating=form.wifi_rating.data, toilet_rating=form.toilet_rating.data,
+                location=form.location.data
+            )
+            db.session.add(new_cafe)
+            db.session.commit()
 
         return redirect(url_for('home'))
     return render_template('new-cafe.html', form=form, path='/insert-new-cafe')
+
+
+@app.route('/cafe/<id>')
+def cafe_details(id):
+    cafe_to_show = Cafe.query.get(id)
+    return render_template('cafe-details.html', cafe=cafe_to_show)
+
+
+@app.route('/delete/<id>', methods=["POST"])
+def delete_cafe(id):
+    if request.method == 'POST':
+        form_data = request.form
+        if form_data['secret_key'] == 'i9s19h8a1n14':
+            cafe_to_del = Cafe.query.get(id)
+            db.session.delete(cafe_to_del)
+            db.session.commit()
+    return redirect('/')
 
 
 if __name__ == '__main__':
